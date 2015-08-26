0989fd67e1c4e9f6562f9128b14754da640d2733
YuyaMurata <kaeru_yuya@hotmail.com>
2015-08-26 17:26:18 +0900

    notePC commit

--- AgentSystem/src/rda/test/MessageQueueTest.java
+++ AgentSystem/src/rda/test/MessageQueueTest.java
@@ -1,44 +1,71 @@
 package rda.test;
 
 import java.util.ArrayList;
+import rda.agent.CreateAgent;
 
 import rda.data.DataGenerator;
+import rda.data.MountData;
 import rda.data.OutputData;
 import rda.queue.MessageObject;
 import rda.queue.ReciveMessageQueue;
+import rda.queue.WindowController;
 
 public class MessageQueueTest{
-	//Experiment Condition
-
-	private static ReciveMessageQueue mq = new ReciveMessageQueue("MQ0");
-
-	private static void send(int run){
-            MessageObject msg;
-            ArrayList<MessageObject> messageList = new ArrayList<MessageObject>();
+    //Experiment Condition
+    private static final int NUMBER_USER = 1;
+    private static final int NUMBER_DATA = 100;
             
-            for(int i=0; i < run; i++){
-		DataGenerator ag = DataGenerator.getInstance();
-                msg = new MessageObject(ag.getData().agentKey, ag.getData().data);
-		
-		messageList.add(msg);
-
-		//sendMessageQueue(mq, message);
-            }
-            
-            System.out.println(messageList.toString());
+    private static void createUser(int numOfUser) {
+        CreateAgent agent = new CreateAgent();
+        agent.create("U#00", numOfUser);
     }
 
-	private static void sendMessageQueue(ReciveMessageQueue messageQueue, Object message){
-		//QueueにPutする
-		messageQueue.putMessage(message);
-	}
+    private static void execute(int run){
+        //ReciveMessageQueue rmq = new ReciveMessageQueue("MQ0");
+        WindowController mq = new WindowController(String.valueOf("Win_Main"));
+        
+        MessageObject msg;
+        ArrayList<MessageObject> messageList = new ArrayList<MessageObject>();
+        ArrayList<MessageObject> oneMessage;
+        DataGenerator ag = DataGenerator.getInstance();
+        
+        for(int i=0; i < run; i++){
+            msg = new MessageObject(ag.getData().agentKey, ag.getData().data);
+            
+            //WindowContoroler to MQ
+            mq.sendMessage(msg);
+            
+            // Direct MessageQueue
+            //oneMessage = new ArrayList<MessageObject>();
+            //rmq.putMessage(oneMessage.add(msg));
+            
+        }
+        
+        mq.close();
+        //rmq.close();
+            
+        System.out.println(messageList.toString());
+    }
 
-	static OutputData out;
-	public static void main(String[] args) {
-            //Update Agent
-            int number = 100;
-            if(!args.equals(""))
-                number = Integer.parseInt(args[0]);
-            send(number);
-	}
+    static OutputData out;
+    public static void main(String[] args) {
+        //Create UserAgents
+        createUser(NUMBER_USER);
+
+        //Update Agent
+        int number = NUMBER_DATA;
+        if(!args.equals(""))
+            number = Integer.parseInt(args[0]);
+        
+        long start = System.currentTimeMillis();
+	System.out.println("Start Agent System : "+start);
+        
+        //Executable Test
+        execute(number);
+        
+        long stop = System.currentTimeMillis();
+        System.out.println("Stop Agent System : "+stop);
+        
+        System.out.println("transaction_time : "+(stop-start));
+    }
 }
