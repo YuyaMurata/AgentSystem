0e71dbaa8e2340371556e556911d8bc76eb4c5e1
YuyaMurata <kaeru_yuya@hotmail.com>
2015-08-28 01:50:36 +0900

    PC Commit

--- AgentSystem/src/rda/property/Property.java
+++ AgentSystem/src/rda/property/Property.java
@@ -5,61 +5,79 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class Property {
-	public Property() {
-		// TODO 自動生成されたコンストラクター・スタブ
-		loadServerProperty();
-		loadQueueProperty();
-		loadAgentProperty();
-	}
+    public Property() {
+        // TODO 自動生成されたコンストラクター・スタブ
+        loadPropertyALL();
+        getValueALL();
+    }
 
-	private static Properties server = new Properties();
-	private static Properties queue = new Properties();
-	private static Properties agent = new Properties();
+    private static final Properties server = new Properties();
+    private static final Properties queue = new Properties();
+    private static final Properties agent = new Properties();
 
-	//Load Property
-	private void load(Properties prop, String filename){
-		System.out.println("Load " + filename);
+    //Load Property
+    private void load(Properties prop, String filename){
+        System.out.println("* Load " + filename);
 
-		try{
-			prop.load(new FileInputStream(filename));
-		}catch(IOException e){
-			e.printStackTrace();
-		}
-	}
+        try{
+            prop.load(new FileInputStream(filename));
+        }catch(IOException e){
+                e.printStackTrace();
+        }
+    }
 
-	//Server Property
-	private void loadServerProperty(){
-		String filename = "property/server.property";
-		load(server, filename);
-	}
+    //Server Property
+    private void loadServerProperty(){
+        String filename = "property/server.property";
+        load(server, filename);
+    }
 
-	//Queue Property
-	private void loadQueueProperty(){
-		String filename = "property/queue.property";
-		load(queue, filename);
-	}
+    //Queue Property
+    private void loadQueueProperty(){
+        String filename = "property/queue.property";
+        load(queue, filename);
+    }
 
-	//Agent Property
-	private void loadAgentProperty(){
-		String filename = "property/agent.property";
-		load(agent, filename);
-	}
+    //Agent Property
+    private void loadAgentProperty(){
+        String filename = "property/agent.property";
+        load(agent, filename);
+    }
 
-	//Get Value
-	public String getValue(String prop, String key){
-		switch (prop) {
-		case "server":
-			System.out.println("ServerProperty Read : "+key+"="+server.getProperty(key));
-			return server.getProperty(key);
-		case "queue":
-			System.out.println("QueueProperty Read : "+key+"="+queue.getProperty(key));
-			return queue.getProperty(key);
-		case "agent":
-			System.out.println("AgentProperty Read : "+key+"="+agent.getProperty(key));
-			return agent.getProperty(key);
-		default:
-			return null;
-		}
-	}
-
+    //Get Value
+    public String getValue(String prop, String key){
+        switch (prop) {
+            case "server": return server.getProperty(key);
+            case "queue" : return queue.getProperty(key);
+            case "agent" : return agent.getProperty(key);
+            default      : return null;
+        }
+    }
+    
+    private void loadPropertyALL(){
+        System.out.println("*************** -Load Property File- ***************");
+        
+        loadServerProperty();
+        loadQueueProperty();
+        loadAgentProperty();
+    }
+    
+    private void getValueALL(){
+        System.out.println("*************** -Get Property Value- ***************");
+        for(Object key: server.keySet())
+            System.out.println("* ServerProperty Value : "+(String)key+"="+getValue("server", (String)key));
+        System.out.println("* ------------------------------------------------ *");
+        
+        for(Object key: queue.keySet())
+            System.out.println("* QueueProperty Value  : "+(String)key+"="+getValue("queue", (String)key));
+        System.out.println("* ------------------------------------------------ *");
+        
+        for(Object key: queue.keySet())
+            System.out.println("* AgentProperty Value  : "+(String)key+"="+getValue("agent", (String)key));
+        System.out.println("****************************************************");
+    }
+    
+    public static void main(String args[]){
+        Property prop = new Property();
+    }
 }
