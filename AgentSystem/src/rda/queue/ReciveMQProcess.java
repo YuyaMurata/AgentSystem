56500d7c63d30e3090870da1aa8952afa4cd3c4a
YuyaMurata <kaeru_yuya@hotmail.com>
2015-08-30 03:48:34 +0900

    PC Commit

--- AgentSystem/src/rda/queue/ReciveMQProcess.java
+++ AgentSystem/src/rda/queue/ReciveMQProcess.java
@@ -39,6 +39,8 @@ public class ReciveMQProcess extends Thread {
         
         while(mq.isRunning() || !mq.isEmpty()){
             try {
+                mqSS.setMQSSMap(name, mq.getSize());
+                
                 ArrayList<MessageObject> msgList  = (ArrayList<MessageObject>) mq.getMessage();
                 //System.out.println(name+"_"+messageList.size()+":稼動中!");
             
@@ -55,8 +57,6 @@ public class ReciveMQProcess extends Thread {
                     for(AgentKey key : msgMap.keySet())
                         user.sendUpdateMessage(key, msgMap.get(key));
                     msgMap.clear();
-                
-                    mqSS.setMQSSMap(name, mq.getSize());
                 }
             } catch (InterruptedException ex) {
             }   
