aa93ddd0be965411d042616b71128027eb64d6e3
YuyaMurata <kaeru_yuya@hotmail.com>
2015-08-30 00:46:26 +0900

    PC Commit

--- AgentSystem/src/rda/queue/ReciveMQProcess.java
+++ AgentSystem/src/rda/queue/ReciveMQProcess.java
@@ -13,20 +13,23 @@ import rda.property.SetProperty;
 public class ReciveMQProcess extends Thread {	
     private final String name;
     private final ReciveMessageQueue mq;
-    private final MessageQueueTimer mqt;
-    private final CreateAgentClient ag = new CreateAgentClient();
+    //private final MessageQueueTimer mqt;
     private static final Marker rMQMarker = MarkerFactory.getMarker("ReciveMessageQueue");
     
     public ReciveMQProcess(String name, ReciveMessageQueue queue) {
+        // TODO 自動生成されたコンストラクター・スタブ
         SetProperty.logger.info(rMQMarker, "********** Recive Message Queue {} Start!! ********** ", name);
         
         this.name = name;
         this.mq = queue;
         
-        this.mqt = MessageQueueTimer.getInstance();
+        //this.mqt = MessageQueueTimer.getInstance();
     }
 
     public void run() {
+        // TODO 自動生成されたメソッド・スタブ
+        CreateAgentClient ag = new CreateAgentClient();
+                
         UpdateUser user = new UpdateUser();
         user.setParam(ag.getClient());
         HashMap<AgentKey, ArrayList<Integer>> msgMap = new HashMap<>();
@@ -42,21 +45,21 @@ public class ReciveMQProcess extends Thread {
                 msgMap.get(msg.agentKey).add(msg.data);
             }
                 
-            if(mq.isEmpty() || mqt.getTimer()){
+            /*if(mq.isEmpty() || mqt.getTimer()){
                 for(AgentKey key : msgMap.keySet())
                     user.sendUpdateMessage(key, msgMap.get(key));
                 msgMap.clear();
                 
                 //MQSpecificStorage.setMQSSMap(name, mq.getSize());
-            }
+            }*/
         }
-        
+		
+        ag.close();
         SetProperty.logger.info(rMQMarker, "********** Recive Message Queue {} Stop!! ********** ", name);
     }
 
     private boolean running = true;
     public void stopRunning(){
         running = false;
-        ag.close();
     }
 }
