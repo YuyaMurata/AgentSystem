f95327de6f71342f2bc279584873957e7adbd81d
YuyaMurata <kaeru_yuya@hotmail.com>
2015-09-16 05:22:15 +0900

    PC Commit

--- AgentSystem/setting/exec_system.sh
+++ AgentSystem/setting/exec_system.sh
@@ -1,6 +1,7 @@
 export CLASSPATH=$CLASSPATH:../property:../resource:../library/Log/logback-access-1.1.3.jar:../library/Log/logback-classic-1.1.3.jar:../library/Log/logback-core-1.1.3.jar:../library/Log/slf4j-api-1.7.12.jar
 
 for i in 1 2 3 do
+
 java -cp $CLASSPATH rda.property.RewriteProperty $i
 
 vmstat -n -S m -a 1 | awk '{print strftime("%H:%M:%S"), $0} { system(":") }' > vmstat.log &
