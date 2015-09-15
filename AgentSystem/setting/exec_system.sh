751a8ddfc6abf595402d0673f86a925ee40ebb65
YuyaMurata <kaeru_yuya@hotmail.com>
2015-09-16 05:16:03 +0900

    PC Commit

--- AgentSystem/setting/exec_system.sh
+++ AgentSystem/setting/exec_system.sh
@@ -1,5 +1,8 @@
 export CLASSPATH=$CLASSPATH:../property:../resource:../library/Log/logback-access-1.1.3.jar:../library/Log/logback-classic-1.1.3.jar:../library/Log/logback-core-1.1.3.jar:../library/Log/slf4j-api-1.7.12.jar
 
+for i in 1 2 3 do
+java -cp $CLASSPATH rda.property.RewriteProperty $i
+
 vmstat -n -S m -a 1 | awk '{print strftime("%H:%M:%S"), $0} { system(":") }' > vmstat.log &
 java -cp $CLASSPATH rda.main.Main
 
@@ -8,12 +11,16 @@ cat vmstat.log | awk '{print $1 "," $14 "," $15}' > vmstat.csv
 
 java -cp $CLASSPATH rda.test.ReadTest
 
-mkdir logs/user
+mkdir logs/user$i
 
-cp logs/*.log logs/user
-cp -r logs/history logs/user
+cp logs/*.csv logs/user$i
+cp -r logs/history logs/user$i
 
 rm -f logs/*.log
 rm -f logs/history/*
 
-mv vmstat.* logs/user
+mv vmstat.* logs/user$i
+
+java -cp $CLASSPATH rda.Dispose
+
+done
