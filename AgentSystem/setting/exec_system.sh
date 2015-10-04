09fd2157f9d2d3ad89e4c741fb527dabded05690
YuyaMurata <kaeru_yuya@hotmail.com>
2015-10-05 03:17:10 +0900

    PC Commit

--- AgentSystem/setting/exec_system.sh
+++ AgentSystem/setting/exec_system.sh
@@ -1,32 +1,34 @@
 #Setting CLASS PATH
 export CLASSPATH=$CLASSPATH:../property:../resource:../library/Log/logback-access-1.1.3.jar:../library/Log/logback-classic-1.1.3.jar:../library/Log/logback-core-1.1.3.jar:../library/Log/slf4j-api-1.7.12.jar:../library/csv/opencsv-3.5.jar
 
-for i in {1..20}
+#{1..20}
+#30 40 50 60 70 80 90 100
+for i in {1..10}
 do
 
-mkdir logs/user$i
+    mkdir logs/user$i
 
-java -cp $CLASSPATH rda.property.RewriteProperty $i
+    java -cp $CLASSPATH rda.property.RewriteProperty $i
 
-vmstat -n -S m -a 1 | awk '{print strftime("%Y-%m-%d %H:%M:%S.000"), $0} { system(":") }' > vmstat.log &
+    vmstat -n -S m -a 1 | awk '{print strftime("%Y-%m-%d %H:%M:%S.000"), $0} { system(":") }' > vmstat.log &
 
-java -cp $CLASSPATH -Xms4096m -Xmx4096m rda.main.Main
-java -cp $CLASSPATH rda.test.ReadTest
+    java -cp $CLASSPATH -Xms4096m -Xmx4096m rda.main.Main
+    java -cp $CLASSPATH rda.test.ReadTest
 
-java -cp $CLASSPATH rda.Dispose
+    java -cp $CLASSPATH rda.Dispose
 
-killall vmstat
-cat vmstat.log | awk '{print $1 " " $2 "," $15 "," $16}' > vmstat.csv
+    killall vmstat
+    cat vmstat.log | awk '{print $1 " " $2 "," $15 "," $16}' > vmstat.csv
 
-cp logs/*.csv logs/user$i
-cp -r logs/history logs/user$i
-cp -r ../property logs/user$i
+    cp logs/*.csv logs/user$i
+    cp -r logs/history logs/user$i
+    cp -r ../property logs/user$i
 
-rm -f logs/*.csv
-rm -f logs/history/*
+    rm -f logs/*.csv
+    rm -f logs/history/*
 
-mv vmstat.* logs/user$i
+    mv vmstat.* logs/user$i
 
-java -cp $CLASSPATH rda.result.ResultsDataForming $i
+    java -cp $CLASSPATH rda.result.ResultsDataForming $i
 
 done
