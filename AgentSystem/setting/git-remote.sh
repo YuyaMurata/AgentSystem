879e177e37e273daea0ee2f77ae79e014a1b839d
YuyaMurata <kaeru_yuya@hotmail.com>
2015-10-10 22:28:46 +0900

    PC Commit

--- AgentSystem/setting/git-remote.sh
+++ AgentSystem/setting/git-remote.sh
@@ -1,3 +1,10 @@
 git pull
+
+sleep 1
+
+if [$1 = "f"]; then
+    rm -fr $CETA_HOME/classes/*
+fi
+
 ant -f ../build.xml
 \cp -fr ../build/classes/* $CETA_HOME/classes
--- AgentSystem/src/rda/data/MountData.java
+++ AgentSystem/src/rda/data/MountData.java
@@ -1,11 +1,7 @@
 package rda.data;
 
-import org.slf4j.Marker;
-import org.slf4j.MarkerFactory;
-import rda.log.AgentSystemLogger;
 import rda.property.SetProperty;
 import rda.queue.MessageObject;
-import rda.queue.MessageQueueTimer;
 
 public class MountData implements SetProperty{
     public final String name = "Mount";
@@ -36,24 +32,4 @@ public class MountData implements SetProperty{
             Long result = n * (n-1) / 2 * DATA_VOLUME;
             return result;
         }
-
-	private static final AgentSystemLogger logger = AgentSystemLogger.getInstance();
-        private static final Marker scheduleMaker = MarkerFactory.getMarker("Main Schedule");
-	public static void main(String[] args) {
-            MountData dataType = new MountData();
-            long t = 0L;
-            
-            MessageObject msg;
-            long total = 0;
-            for(long i=0; i < TIME_RUN/2; i++){
-                while((msg = DATA_TYPE.getTimeToData(i)) != null){
-                    total = total + 1;
-                }
-                logger.print(scheduleMaker, 
-                "Experiment Step : {} [{}ms]", new Object[]{i, TIME_PERIOD});
-                System.out.println("total_i="+total);
-            }
-            System.out.println("Total:"+total);
-            System.out.println("Data.N:"+getAmountData());
-	}
 }
