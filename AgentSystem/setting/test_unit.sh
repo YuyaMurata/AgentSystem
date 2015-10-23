1168050517e739f6bccbe5643d69d9744cf5d7ee
YuyaMurata <kaeru_yuya@hotmail.com>
2015-10-23 22:43:25 +0900

    PC Commit

--- AgentSystem/setting/test_unit.sh
+++ AgentSystem/setting/test_unit.sh
@@ -2,8 +2,6 @@
  #Program setting file
 export CLASSPATH=$CLASSPATH:../library/pool/commons-pool2-2.4.2.jar
 
-echo $1
-
 case "$1" in
-  "CreateAgentTest" ) echo "Execute Java Program";;
+  "CreateAgentTest" ) java -cp $CLASSPATH rda.test.CreateAgent $2;;
 esac
