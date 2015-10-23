583876d9907feda803e79b9a45ba12dec6d95031
YuyaMurata <kaeru_yuya@hotmail.com>
2015-10-23 22:36:51 +0900

    PC Commit

--- AgentSystem/setting/test_unit.sh
+++ AgentSystem/setting/test_unit.sh
@@ -5,14 +5,6 @@ export CLASSPATH=$CLASSPATH:../library/pool/commons-pool2-2.4.2.jar
 echo $1
 
 case "$1" in
-  1)
-    echo "[INFO] enter numric value is 1";;
-  [23])
-    echo "[INFO] enter numric value is 2 or 3";;
-  [4-9])
-    echo "[INFO] enter numric value is [4-9]";;
-  10)
-    echo "[INFO] enter numric value is 10";;
-  *)
-    echo "[ERR] invalid value";;
+  "CreateAgentTest" ) echo "Execute Java Program";;
+    
 esac
