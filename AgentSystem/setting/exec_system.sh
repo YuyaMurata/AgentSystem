d70360dff6330913666075a35ef9ef31a91bdee6
YuyaMurata <kaeru_yuya@hotmail.com>
2015-10-23 14:28:34 +0900

    PC Commit

--- AgentSystem/setting/exec_system.sh
+++ AgentSystem/setting/exec_system.sh
@@ -5,7 +5,7 @@ LOG_FD=user
 
 #Setting CLASS PATH
  #Program setting file
-export CLASSPATH=$CLASSPATH:../property:../resource
+export CLASSPATH=$CLASSPATH:../property:../resource:../library/pool/commons-pool2-2.4.2.jar
  #Log setting file
 export CLASSPATH=$CLASSPATH:../library/Log/logback-access-1.1.3.jar:../library/Log/logback-classic-1.1.3.jar:../library/Log/logback-core-1.1.3.jar:../library/Log/slf4j-api-1.7.12.jar
  #CSV setting file
