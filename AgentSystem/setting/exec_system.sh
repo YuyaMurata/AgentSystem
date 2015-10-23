43cf713cdc7ddba9bf4dbe9513168aba67c44c32
YuyaMurata <kaeru_yuya@hotmail.com>
2015-10-24 02:53:54 +0900

    PC Commit

--- AgentSystem/setting/exec_system.sh
+++ AgentSystem/setting/exec_system.sh
@@ -15,6 +15,7 @@ export CLASSPATH=$CLASSPATH:../library/csv/opencsv-3.5.jar
 #20 30 40 50 60 70 80 90 100
 #200 300 400 500 600 700 800 900 1000
 #2000 3000 4000 5000 6000 7000 8000 9000 10000
+#Experimental
 for i in 10
 do
 
