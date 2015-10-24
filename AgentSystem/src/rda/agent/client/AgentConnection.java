c429caa6911b43fc67a4be7fc07fcaca2d26b080
YuyaMurata <kaeru_yuya@hotmail.com>
2015-10-24 21:59:29 +0900

    PC Commit

--- AgentSystem/src/rda/agent/client/AgentConnection.java
+++ AgentSystem/src/rda/agent/client/AgentConnection.java
@@ -19,7 +19,7 @@ public class AgentConnection {
     private static final AgentConnection connector = new AgentConnection();
     
     private AgentConnection(){
-        Integer max = 16;
+        Integer max = 8;
         GenericObjectPoolConfig conf = new GenericObjectPoolConfig();
         conf.setMaxIdle(max);
         conf.setMaxTotal(max);
