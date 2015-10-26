f3c818193526daf8f4cfacaffb01f8fdf38c1c4e
YuyaMurata <kaeru_yuya@hotmail.com>
2015-10-27 04:32:05 +0900

    PC Commit

--- AgentSystem/src/rda/queue/HashToMQN.java
+++ AgentSystem/src/rda/queue/HashToMQN.java
@@ -9,7 +9,8 @@ public class HashToMQN implements SetProperty{
 	private static final int HASH_MOD = 9973;
 
 	public static int toMQN(AgentKey agentKey){
-            return ((Math.abs(agentKey.hashCode()) % HASH_MOD) % SERVER_THREAD) % NUMBER_OF_QUEUE;
+            //return ((Math.abs(agentKey.hashCode()) % HASH_MOD) % SERVER_THREAD) % NUMBER_OF_QUEUE;
+            return Math.abs(agentKey.hashCode()) % NUMBER_OF_QUEUE;
 	}
 
 }
