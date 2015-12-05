9a78833b0799ef00c2f765d1c4d4725091e9b7ac
YuyaMurata <kaeru_yuya@hotmail.com>
2015-12-05 16:40:13 +0900

    PC Commit

--- AgentSystem/src/rda/test/UnitTest.java
+++ AgentSystem/src/rda/test/UnitTest.java
@@ -18,7 +18,7 @@ public class UnitTest {
     }
     
     private void createTest(){
-        String userID = "U#000-1-2-3-4-56";
+        String userID = "U#000-1-2-3-4-567";
         new CreateAgentTest().createAgent(userID);
     }
     
