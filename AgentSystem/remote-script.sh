7cf470849122e135380f69b94ae0ea3ae4055f91
YuyaMurata <kaeru_yuya@hotmail.com>
2015-12-11 02:31:57 +0900

    PC Commit

--- AgentSystem/remote-script.sh
+++
@@ -1,6 +0,0 @@
-zip -r `hostname`_`date +%Y%m%d%H%M%S`.zip logs
-
-#h1‚Ìdropbox‚Éƒtƒ@ƒCƒ‹‚ðˆÚ“®‚·‚é
-sshpass -p 11m35584 scp `hostname`_*.zip root@h1:$CETA_HOME/App/dropbox_log
-
-rm -f `hostname`_*.zip
