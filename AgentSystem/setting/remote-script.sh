f7c3e10644acf4ea78e3c35f08e90a6acdca7544
YuyaMurata <kaeru_yuya@hotmail.com>
2015-12-11 02:51:35 +0900

    PC Commit

--- AgentSystem/setting/remote-script.sh
+++ AgentSystem/setting/remote-script.sh
@@ -1,4 +1,5 @@
-zip -r `hostname`_`date +%Y%m%d%H%M%S`.zip "logs"
+ls -l
+zip -r `hostname`_`date +%Y%m%d%H%M%S`.zip logs
 
 #h1のdropboxにファイルを移動する
 #sshpass -p 11m35584 scp `hostname`_*.zip root@h1:$CETA_HOME/App/dropbox_log
