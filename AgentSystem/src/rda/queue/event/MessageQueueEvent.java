739a2d6b6b0a38d22e8a412073127f7502b0083f
YuyaMurata <kaeru_yuya@hotmail.com>
2015-12-13 10:47:36 +0900

    PC Commit

--- AgentSystem/src/rda/queue/event/MessageQueueEvent.java
+++ AgentSystem/src/rda/queue/event/MessageQueueEvent.java
@@ -5,28 +5,31 @@ import org.slf4j.MarkerFactory;
 import rda.log.AgentSystemLogger;
 import rda.queue.manager.MessageQueueManager;
 
-public class MessageQueueEvent extends Exception{
+public class MessageQueueEvent extends Exception implements Runnable{
     /**
     *
     */
     private static final Marker dataMarker = MarkerFactory.getMarker("data");
     private static final AgentSystemLogger logger = AgentSystemLogger.getInstance();
+    
+    private MessageQueueManager manager = MessageQueueManager.getInstance();
+    
     private final String name;
     public MessageQueueEvent(String name) {
         super(name);
         this.name = name;
+        
+        new Thread(this).start();
     }
 
     public void printEvent(){
         //イベント出力
         logger.printMQEvent(dataMarker, 
                 "MQName_,{}, : ########## 負荷検知 ########## ", new String[]{name});
-        
-        decompose();
     }
-    
-    public void decompose(){
-        MessageQueueManager manager = MessageQueueManager.getInstance();
+
+    @Override
+    public void run() {
         manager.decompose(name);
     }
 }
