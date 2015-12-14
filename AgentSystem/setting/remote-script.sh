815faed42aaafac93a0c269388d79297c250a83d
YuyaMurata <kaeru_yuya@hotmail.com>
2015-12-15 04:47:25 +0900

    PC Commit

--- AgentSystem/setting/remote-script.sh
+++ AgentSystem/setting/remote-script.sh
@@ -1,7 +1,20 @@
-zip -r `hostname`_`date +%Y%m%d%H%M%S`.zip logs
+#環境設定
+./git-remote.sh
+chmod 777 *.sh
 
-#h1のdropboxにファイルを移動する
+#サーバーの起動
+./start.sh
+sleep 15
+
+./exec_system.sh 10
+
+#Logを圧縮し, h1のdropboxにファイルを移動する
+zip -r `hostname`_`date +%Y%m%d%H%M%S`.zip logs
 sshpass -p 11m35584 scp `hostname`_*.zip root@h1:$CETA_HOME/App/dropbox_log
 
+#生成ファイルの削除
 rm -f `hostname`_*.zip
 rm -fR logs/user*
+
+#サーバーの終了
+./stop.sh
--- AgentSystem/setting/remote-start.sh
+++ AgentSystem/setting/remote-start.sh
@@ -3,37 +3,12 @@ sshpass -p 11m35584 ssh root@$1 'echo `hostname`'
 echo "確認後 Enter"
 read
 
-#サーバーの起動
-sshpass -p 11m35584 ssh root@$1 \
-"source /etc/profile;\
-cd $CETA_HOME/App/AgentSystem/AgentSystem/setting;\
-chmod 777 *.sh;\
-./git-remote.sh;\
-chmod 777 *.sh;\
-./start.sh"
-
-sleep 15
-
-#プログラムの実行
-sshpass -p 11m35584 ssh root@$1 \
-"source /etc/profile;\
-cd $CETA_HOME/App/AgentSystem/AgentSystem/setting;\
-chmod 777 *.sh;\
-./exec_system.sh 10"
-
-#生成されたファイルを送る
+#遠隔で実験用スクリプトを起動
 sshpass -p 11m35584 ssh root@$1 \
 "source /etc/profile;\
 cd $CETA_HOME/App/AgentSystem/AgentSystem/setting;\
 chmod 777 *.sh;\
 ./remote-script.sh"
-
-#サーバーの終了
-sshpass -p 11m35584 ssh root@$1 \
-"source /etc/profile;\
-cd $CETA_HOME/App/AgentSystem/AgentSystem/setting;\
-chmod 777 *.sh;\
-./stop.sh"
 
 #ログをDropboxに送る
 dropbox upload $CETA_HOME/App/dropbox_log/*.zip logs
