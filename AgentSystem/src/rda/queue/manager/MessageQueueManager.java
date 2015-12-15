633b7db427f87ec093dcd728beb1a985a2d1af5b
YuyaMurata <kaeru_yuya@hotmail.com>
2015-12-15 16:56:44 +0900

    notePC commit

--- AgentSystem/src/rda/queue/manager/MessageQueueManager.java
+++ AgentSystem/src/rda/queue/manager/MessageQueueManager.java
@@ -63,6 +63,7 @@ public class MessageQueueManager {
     
     public ReciveMessageQueue getMessageQueue(String uid){
         int sid = id.ageToSID(uid);
+        System.out.println("RECIVE::"+uid+"_SID="+sid+"---AGID="+id.sidToAGID(sid));
         return messageQueue.get(sid);
     }
     
