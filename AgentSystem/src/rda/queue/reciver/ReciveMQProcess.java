0ca90b572cc9fcc2f54935514d88c9ba48c0b0c2
YuyaMurata <kaeru_yuya@hotmail.com>
2015-12-15 18:45:59 +0900

    notePC commit

--- AgentSystem/src/rda/queue/reciver/ReciveMQProcess.java
+++ AgentSystem/src/rda/queue/reciver/ReciveMQProcess.java
@@ -36,7 +36,7 @@ public class ReciveMQProcess extends Thread{
                     if(!mq.isRunning()) break;
                 }
                 
-                if(mq.getMessage() == null) continue;
+                if(mq.getMessage() != null){
                 
                 for(MessageObject msg : (ArrayList<MessageObject>)mq.getMessage()){
                     dataList.add(msg.data);
@@ -45,6 +45,7 @@ public class ReciveMQProcess extends Thread{
                 if(mq.isEmpty() || mqt.getTimer()){
                     user.sendUpdateMessage(key, dataList);
                     dataList.clear();
+                }
                 }
             } catch (InterruptedException e) {}
         }
