55133cd3e45d2796c48d65496b98e1a44159c72d
YuyaMurata <kaeru_yuya@hotmail.com>
2016-01-02 00:31:27 +0900

    PC Commit

--- AgentSystem/src/rda/main/Main.java
+++ AgentSystem/src/rda/main/Main.java
@@ -105,11 +105,10 @@ public class Main implements SetProperty, SetDataType{
             Thread.sleep(TIME_RUN*1000+TIME_DELAY);
         } catch (InterruptedException ex) {
         } finally{
-            mainTask.shutdown();
-            loggingTask.shutdown();
-            
             fMap.mainFuture.cancel(true);
+            mainTask.shutdownNow();
             fMap.logFuture.cancel(true);
+            loggingTask.shutdownNow();
             
             MessageQueueTimer.getInstance().close();
             MessageQueueManager.getInstance().stopAll();
