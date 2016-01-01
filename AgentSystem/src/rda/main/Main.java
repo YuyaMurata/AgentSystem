340755074054dee2be323147657f1fbfbbd0a4cd
YuyaMurata <kaeru_yuya@hotmail.com>
2016-01-02 00:07:23 +0900

    PC Commit

--- AgentSystem/src/rda/main/Main.java
+++ AgentSystem/src/rda/main/Main.java
@@ -5,8 +5,6 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.slf4j.Marker;
 import org.slf4j.MarkerFactory;
 import rda.data.SetDataType;
@@ -15,7 +13,6 @@ import rda.log.AgentSystemLogger;
 import rda.property.SetProperty;
 import rda.queue.id.IDToMQN;
 import rda.queue.manager.MessageQueueManager;
-import rda.queue.timer.MessageQueueTimer;
 import rda.window.WindowController;
 
 public class Main implements SetProperty, SetDataType{
