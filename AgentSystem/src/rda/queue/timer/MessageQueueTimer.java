792fea8a1a4a7a8f514c9163fcb6d16d001bccf1
YuyaMurata <kaeru_yuya@hotmail.com>
2016-01-22 12:30:32 +0900

    notePC commit

--- AgentSystem/src/rda/queue/timer/MessageQueueTimer.java
+++ AgentSystem/src/rda/queue/timer/MessageQueueTimer.java
@@ -31,9 +31,10 @@ public class MessageQueueTimer implements Runnable, SetProperty{
     @Override
     public void run() {
         binaryTimer = !binaryTimer;
+        System.out.println("******** Thread Print ********");
         for(Thread t : Thread.getAllStackTraces().keySet()){
             if(t.getName().contains("R#")) continue;
-            System.out.println("Thread Stats::"+t.getName()+"="+t.getState());
+            System.out.println("Thread Stats::"+t.getName()+"="+t.getState()+" num="+Thread.getAllStackTraces().size());
         }
     }
     
