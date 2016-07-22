0ca895d2538e808f320b53da985cfa4c87ee2fdf
kaeru <kaeru@kaeru-PC>
2016-07-22 22:48:06 +0900

    PC Commit

---
+++ AgentSystem/src/test/property/PropertyTestOut.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.property;
+
+import rda.property.Property;
+
+
+/**
+ *
+ * @author kaeru
+ */
+public class PropertyTestOut{
+    public static void main(String[] args) {
+        System.out.println("Property Print Out");
+        Property.main(args);
+    }
+}
